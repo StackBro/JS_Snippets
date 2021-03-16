135a9876c3f6b8ec61d751a434d94a15a888bd4c
dogram99 <rewq_gol_47@mail.ru>
2021-03-16 12:24:52 +0300

    adding folder alternative-entry and new js file in the folder

---
+++ alternative-entry/alternative-entry.js
@@ -0,0 +1,24 @@
+function test1() {
+    /**
+     * Напишите функцию, которая принимает строку и из строчных букв делает прописные, а прописные - строчными.
+     */
+    console.log(alternativeEntry('hello world'))    // => 'HELLO WORLD'
+    console.log(alternativeEntry('HELLO WORLD'))   // => 'hello world'
+    console.log(alternativeEntry('12345'))        // => '12345'
+    console.log(alternativeEntry('1a2b3c4d5e'))  // => '1A2B3C4D5E'
+}
+
+function alternativeEntry(word = '') {
+    const str = word.toString();
+    const arr = str.split('')
+
+    const newArr = arr.map((symbol) => {
+        const isUpperCase = (symbol === symbol.toUpperCase());
+        return isUpperCase ? symbol.toLowerCase() : symbol.toUpperCase();
+    })
+
+    return newArr.join('')
+}
+
+test1();
+
