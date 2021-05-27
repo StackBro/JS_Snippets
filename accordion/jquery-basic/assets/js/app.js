4c96ca3a54040c3c2cc24feac37889c3a145582c
Pavel Shabanov <pshabanov@nurtelecom.kg>
2021-05-27 17:36:59 +0600

    accordion on jquery

---
+++ accordion/jquery-basic/assets/js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    let accordion = function (){
+        let data = $('.accordion').attr('data-accordion');
+
+        $('.accordion-header').on('click', function(){
+            if(data === 'close'){
+                $('.accordion-body').slideUp();
+                if($(this).hasClass('active')){
+                    $(this).toggleClass('active');
+                }
+                else{
+                    $('.accordion-header').removeClass('active');
+                    $(this).toggleClass('active');
+                }
+            }
+            else{
+                $(this).toggleClass('active');
+            }
+            $(this).next('.accordion-body').not(':animated').slideToggle();
+        });
+    }
+    accordion();
+});
